da41bea394baec1941df8d7487627a5fb78007a4
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-11-25 03:58:56 +0900

    spring boot init

---
+++ whattoeat/src/main/java/com/eatlunch/whattoeat/WhattoeatApplication.java
@@ -0,0 +1,13 @@
+package com.eatlunch.whattoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattoeatApplication.class, args);
+	}
+
+}
---
+++ whattoeat/src/test/java/com/eatlunch/whattoeat/WhattoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatlunch.whattoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
